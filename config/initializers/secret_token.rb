6edec57f1653526e3fb45d1850771a70cba913b0
odesey <odesey@gmail.com>
2013-07-14 19:53:52 -0400

    intial rails project for musical element with bacbone and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicElementBb::Application.config.secret_token = '9be43267b44aed1a8190e5e9efab246d9cb107ec9707755019ad578291641d5458a08c70ef68afa3bd4a3fc0e86fa4c4939256aebf4dc1ddc52d11d098fcab4d'
